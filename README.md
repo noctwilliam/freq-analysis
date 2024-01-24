a5c4493cf89050014a1068f3eb619a14e672ede8
Harith A. <harith2312@gmail.com>
2024-01-24 17:00:40 +0800

    substitution table not done yet

---
+++ README.md
@@ -0,0 +1,3 @@
+#### CSC669 Project
+
+This project is a simple implementation of frequency analysis using web application with some UI from Bootstrap on CDN
--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 function getLetterFrequency(str) {
 	str = removeWhitespaces(str)
 	const frequencyMap = new Map();
+	const totalLetters = str.length;
 	for (let i = 0; i < str.length; i++) {
 		const letter = str[i];
 		if (frequencyMap.has(letter)) {
@@ -12,9 +13,9 @@ function getLetterFrequency(str) {
 	const sortedMap = new Map([...frequencyMap.entries()].sort((a, b) => b[1] - a[1]));
 
 	let mapString = Array.from(sortedMap)
-		.map(([key, value]) => `${key}: ${value}`)
+		.map(([key, value]) => `${key}: ${(value / totalLetters * 100).toFixed(2)}%`)
 		.join("\n");
-	
+	console.log(mapString);
 	return mapString;
 }
 
@@ -22,21 +23,59 @@ function removeWhitespaces(str) {
 	return str.replace(/\s/g, '');
 }
 
-function getLettersWithFrequency(frequency, str) {
-	const letterFrequencyMap = getLetterFrequency(removeWhitespaces(str));
-	const lettersWithFrequency = new Map();
-	for (const [letter, freq] of letterFrequencyMap) {
-		if (freq === frequency) {
-			lettersWithFrequency.set(letter, freq);
+function countSingle(str){
+	let singCount = {};
+	for (let i = 0; i < str.length; i++) {
+		let letter = str[i];
+		if (singCount[letter]) {
+			singCount[letter]++;
+		} else {
+			singCount[letter] = 1;
 		}
 	}
-	let mapString = Array.from(lettersWithFrequency)
-	.map(([key, value]) => `${key}: ${value}`)
-	.join("\n");
-	if (mapString === "") {
-		mapString = "No letters with that frequency";
-	}
-	console.log(mapString);
-	return mapString;
+	let countString = Object.entries(singCount)
+		.map(([key, value]) => `${key}: ${value}`)
+		.join("\n");
+
+	console.log(countString);
+	return countString;
 }
 
+
+function countDigrams(str) {
+	let digramCounts = {};
+	for (let i = 0; i < str.length - 1; i++) {
+		let digram = str.substring(i, i + 2);
+		if (digramCounts[digram]) {
+			digramCounts[digram]++;
+		} else {
+			digramCounts[digram] = 1;
+		}
+	}
+
+	let digramString = Object.entries(digramCounts)
+		.map(([key, value]) => `${key}: ${value}`)
+		.join("\n");
+
+	return digramString;
+}
+
+function countTrigrams(str) {
+	let trigramCounts = {};
+	for (let i = 0; i < str.length - 2; i++) {
+		let trigram = str.substring(i, i + 3);
+		if (trigramCounts[trigram]) {
+			trigramCounts[trigram]++;
+		} else {
+			trigramCounts[trigram] = 1;
+		}
+	}
+
+	let trigramString = Object.entries(trigramCounts)
+	.map(([key, value]) => `${key}: ${value}`)
+	.join("\n");
+
+	return trigramString;
+}
+
+countSingle("Muhammad Harith Azmi");
